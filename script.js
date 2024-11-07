269f74b91169068afc5ee449073aa1852f7c3f7d
Michael Spari <michaelspari72@gmail.com>
2024-11-07 08:05:20 +0100

    Kommentarfunktion eingefügt

--- script.js
+++ script.js
@@ -2,39 +2,63 @@ let bookImg = [{
     "cover": './assets/img/thumb.png'
 }];
 
+// let allNotes = {
+//     commentname: ['Ba', 'Aufgabe'],
+//     notes: ['banana','rasen mähen'],
+//     trashNotesTitles: [],
+//     trashNotes: []
+// }
+
 // function mit for loop und ausgabe von bookTemplate(book) in id="bookFrame"
 function renderBooks() {
     const bookFrame = document.getElementById("bookFrame");
+    bookFrame.innerHTML = "";
     for (let index = 0; index < books.length; index++) {
         const book = books[index];
         bookFrame.innerHTML += bookTemplate(book);
     }
 }
 
-function commentsTemplate(comment) {
-    return `<div>
-    <p><b>${comment.name}</b>: ${comment.comment}</p></div>`;
+function bookTemplate(book) {
+    let commentsData = '';
+    for (let i = 0; i < book.comments.length; i++) {
+        const comment = book.comments[i];
+        commentsData += `<b>${comment.name}:</b><br>${comment.comment}<br><br>`;
+    }
+    return `<div class="contentBookFrame">
+        <h3>${book.name}</h3>
+        <hr>
+        <img src="${bookImg[0].cover}">
+        <hr>
+        <p>${book.price} ${book.liked}</p>
+        <p><b>Autor: </b>${book.author}</p>
+        <p><b>Erscheinungsjahr: </b>${book.publishedYear}</p>
+        <p><b>Genre: </b>${book.genre}</p>
+        <hr>
+        <p><b>Kommentare:</b></p><br><div class="commentFrame">${commentsData}</div>
+        <hr>
+        <div class="commentInterface">
+                <label for="name">Name:</label>
+                <input id="name" type="text" placeholder="Name eingeben...">
+                <label for="comment">Kommentar:</label>
+                <textarea id="comment" type="text" rows="3" placeholder="Kommentar eingeben..."></textarea>
+                <button onclick="addComment()">Kommentar senden</button>
+                </div></div>`;
 }
 
-function bookTemplate(book) {
-    return `<div class="contentBookFrame">
-    <h3>${book.name}</h3>
-    <hr>
-    <img src="${bookImg[0].cover}">
-    <hr>
-    <p>${book.price} ${book.liked}</p>
-    <p><b>Autor: </b>${book.author}</p>
-    <p><b>Erscheinungsjahr: </b>${book.publishedYear}</p>
-    <p><b>Genre: </b>${book.genre}</p>
-    <hr>
-    <p><b>Kommentare:</b></p><br><div class="commentFrame"> ${book.comments.map(comment => commentsTemplate(comment)).join('')}</div></div>`;
-    
-    // return a string of HTML containing the book data
-    // titel
-    // bild vom buch
-    // preis liked
-    // author
-    // published year
-    // genre
-    // comments
+function addComment(){
+    let nameInputRef = document.getElementById('name');
+    let nameInput = nameInputRef.value;
+    let commentarInputRef = document.getElementById('comment');
+    let commentarInput = commentarInputRef.value;
+    // if (!nameInput || !commentarInput) {
+    //     alert("Bitte Name und Kommentar eingeben");
+    //     return;
+    // }
+    const bookIndex = books[index];
+    books[bookIndex].comments.push({ name: nameInput, comment: commentarInput });
+    renderBooks();
+    nameInputRef.value = "";
+    commentarInputRef.value = "";
+
 }
